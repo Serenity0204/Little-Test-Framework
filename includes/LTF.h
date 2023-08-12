002da7845e308fbd93013c19da4e92f241b0058b
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-08-12 10:18:25 -0700

    added basic template

--- includes/LTF.h
+++ includes/LTF.h
@@ -4,4 +4,81 @@
 #pragma once
 #include "stub/stub.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+class TestFramework
+{
+public:
+    struct TestResult
+    {
+        std::string test_name;
+        bool passed;
+    };
+
+    void addTest(std::string name, void (*testFunction)())
+    {
+        tests.push_back({name, testFunction});
+    }
+
+    void runTests()
+    {
+        int numPassed = 0;
+        int numFailed = 0;
+
+        for (const auto& test : tests)
+        {
+            try
+            {
+                test.testFunction();
+                std::cout << "[PASS] " << test.name << std::endl;
+                numPassed++;
+            }
+            catch (const std::exception& e)
+            {
+                std::cout << "[FAIL] " << test.name << " - " << e.what() << std::endl;
+                numFailed++;
+            }
+        }
+
+        std::cout << "\n=== Test Summary ===\n";
+        std::cout << "Total tests: " << tests.size() << std::endl;
+        std::cout << "Passed: " << numPassed << std::endl;
+        std::cout << "Failed: " << numFailed << std::endl;
+    }
+
+private:
+    struct Test
+    {
+        std::string name;
+        void (*testFunction)();
+    };
+
+    std::vector<Test> tests;
+};
+
+// Assertion Macros
+
+#define ASSERT_TRUE(condition) \
+    if (!(condition)) throw std::runtime_error("Condition is false: " #condition)
+
+#define ASSERT_EQUAL(expected, actual) \
+    if ((expected) != (actual)) throw std::runtime_error("Expected: " #expected " | Actual: " #actual)
+
+// Example Test Cases
+
+void testAddition()
+{
+    int result = 2 + 3;
+    ASSERT_EQUAL(5, result);
+}
+
+void testSubtraction()
+{
+    int result = 8 - 4;
+    ASSERT_EQUAL(4, result);
+}
+
+
 #endif // LTF_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+#include "includes/LTF.h"
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-    cout << "hello world" << endl;
+    TestFramework testFramework;
+
+    testFramework.addTest("Addition Test", testAddition);
+    testFramework.addTest("Subtraction Test", testSubtraction);
+
+    testFramework.runTests();
+
     return 0;
 }
